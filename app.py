c0700dab7c144854e1e51fee947922b7e81985f3
Sneha-hash0910 <hme55369@gmail.com>
2026-04-12 17:27:08 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,12 +1,35 @@
 import streamlit as st
+from PyPDF2 import PdfReader
 from agent import run_agent
 
-st.set_page_config(page_title="AI Training Assistant", page_icon="🤖")
+st.title("AI Training Assistant 🤖")
 
-st.title("🤖 AI Training Assistant")
+uploaded_file = st.file_uploader("Upload PDF", type="pdf")
 
-user_input = st.text_input("Ask something:")
+pdf_text = ""
+
+if uploaded_file:
+    reader = PdfReader(uploaded_file)
+    for page in reader.pages:
+        pdf_text += page.extract_text()
+
+    st.success("PDF uploaded successfully!")
+
+user_input = st.text_input("Ask something")
 
 if user_input:
-    response = run_agent(user_input)
+    if pdf_text:
+        # 🔥 Ask from PDF
+        full_input = f"""
+Answer based on this document:
+
+{pdf_text}
+
+Question:
+{user_input}
+"""
+        response = run_agent(full_input)
+    else:
+        response = run_agent(user_input)
+
     st.write(response)
